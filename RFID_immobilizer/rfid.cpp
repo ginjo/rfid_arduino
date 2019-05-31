57f2762a6cc043c0e14ccdcc92b78a7c2c389f2d
wbr <wbr@mac.com>
2019-05-30 21:26:50 -0700

    Create final_index var (raw-tag-len minus 1) for RFID loop.

--- RFID_immobilizer/rfid.cpp
+++ RFID_immobilizer/rfid.cpp
@@ -23,12 +23,14 @@
       Serial.print(")");
       Serial.print(buff[buff_index]);
 
+      int final_index = RAW_TAG_LENGTH - 1;
+
       if (buff_index == 0 && buff[0] != 2) { // reset bogus read
         resetBuffer();
-      } else if (buff_index == 13 && buff[13] != 3) { // reset bogus read
+      } else if (buff_index == final_index && buff[final_index] != 3) { // reset bogus read
         resetBuffer();
         Serial.println("");
-      } else if (buff_index < 13) { // good read, add comma to log and keep reading
+      } else if (buff_index < final_index) { // good read, add comma to log and keep reading
         buff_index++;
         Serial.print(",");
       } else { // tag complete, now process it
@@ -45,7 +47,7 @@
 
   void RFID::resetBuffer() {
     buff_index = 0;
-    strncpy(buff, NULL, 14);
+    strncpy(buff, NULL, RAW_TAG_LENGTH);
   }
 
   
