4bf92eed09597b5e62a0aee7c1fd1523b1393f90
wbr <wbr@mac.com>
2020-02-13 01:35:57 -0800

    Binaries of v0.1.0.pre202.

--- logger.h
+++ logger.h
@@ -36,11 +36,11 @@
   #define DEBUG        // master debug control
   
   #ifdef DEBUG
-    //#define INO_DEBUG    // main ino file
+    #define INO_DEBUG    // main ino file
     //#define BK_DEBUG   // blinker
     //#define CT_DEBUG   // controller
     //#define RD_DEBUG   // reader
-    #define MU_DEBUG   // menu
+    //#define MU_DEBUG   // menu
     //#define SK_DEBUG   // stack
     //#define ST_DEBUG   // settings
     //#define SO_DEBUG   // storage
